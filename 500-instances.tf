9857cf694abc77c012b3d15443a6b4d23bbd9bda
Jon Ball <jonathon.ball@gmail.com>
2019-03-03 12:06:38 -0500

    added volume to instance

--- 500-instances.tf
+++ 500-instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "factorio_server" {
   ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   key_name        = "${aws_key_pair.jball_key_pair.key_name}"
   security_groups = [ "${aws_security_group.game.name}" ]
 
@@ -11,4 +11,19 @@ resource "aws_instance" "factorio_server" {
 
 resource "aws_eip" "game_eip" {
   instance = "${aws_instance.factorio_server.id}"
+}
+
+resource "aws_ebs_volume" "save_games_ebs_volume" {
+  availability_zone = "${aws_instance.factorio_server.availability_zone}"
+  size              = 2
+
+  tags = {
+    Name = "factorio_saves"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.save_games_ebs_volume.id}"
+  instance_id = "${aws_instance.factorio_server.id}"
 }
