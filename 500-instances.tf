ac05089a8b48cbcb107da17744f93db8d4a8fc88
Jon Ball <jonathon.ball@gmail.com>
2019-03-03 00:07:00 -0500

    more spacing fixes

--- 500-instances.tf
+++ 500-instances.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "factorio_server" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.jball_key_pair.key_name}"
-
+  ami             = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.micro"
+  key_name        = "${aws_key_pair.jball_key_pair.key_name}"
   security_groups = [ "${aws_security_group.game.name}" ]
 
   tags = {
