17001a37d02128e5907b93029fd6706cc8e47b21
Jon Ball <jonathon.ball@gmail.com>
2019-03-08 21:05:52 -0500

    removed uneeded ami

--- 100-ami.tf
+++ 100-ami.tf
@@ -1,13 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-}
-
 data "aws_ami" "amazon" {
   most_recent = true
   owners      = ["137112412989"] # Amazon
